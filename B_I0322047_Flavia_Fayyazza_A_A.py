bf5c1285ba59d3512f36794d1ef44fba49ebc7bc
flaviafayya <fayyazzaflavia@gmail.com>
2023-04-11 19:53:55 +0700

    menegrjakan tugas 5

---
+++ B_I0322047_Flavia_Fayyazza_A_A.py
@@ -0,0 +1,41 @@
+A = 4
+B = 3
+C = 2
+D = 1
+E = 0
+
+NIM = {"TI001", "TI002", "TI003", "TI004", "TI005"}
+NAME = {"ALI", "BUDI", "CACA", "DODO", "ELO"}
+
+nilai = {
+    {'ALI'
+     {'fisika'== 80,
+      'biologi' == 80,
+      'matematika' == 85
+      } 
+    {'BUDI'
+     {'fisika' == 70,
+      'biologi' == 60,
+      'matematika' == 70
+      }
+    {'CACA'
+     {'fisika' == 75,
+      'biologi' == 80,
+      'matematika' == 75
+      }
+    {'DODO'
+     {'fisika' == 85,
+      'biologi' == 70,
+      'matematika' == 80
+      }
+    {'ELO'
+     {'fisika' == 90,
+      'biologi' == 60,
+      'matematika' == 80
+      }
+    }
+    }
+    }
+    }
+    }
+}
